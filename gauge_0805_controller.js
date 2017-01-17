5b2ad6225d51bc828ba58a4edb9071a7887cb3a9
alak161 <alak161@yahoo.com>
2017-01-17 13:45:02 -0500

    Update gauge_0805_controller.js

--- gauge_0805_controller.js
+++ gauge_0805_controller.js
@@ -131,7 +131,9 @@ define(function (require) {
                    .style("font-size", "16px")
                    .attr('x',135)
                    .attr('y',200)
-                   .text(newValue.toFixed(2))+'%');  // .text(((newValue.toFixed(2))/100)+'%');
+                   .text(function(newValue) { return newValue/10; });
+          //         .text(newValue.toFixed(2))+'%');  
+          // .text(((newValue.toFixed(2))/100)+'%');
 
         }
       	that.render = render;
